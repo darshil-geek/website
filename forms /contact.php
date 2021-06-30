fc46c70e29400a9310ac8c760a3fac4d271f8935
Darshil Shah <58663553+darshil-geek@users.noreply.github.com>
2021-06-30 10:08:32 +0530

    trying to make the contact form work

--- forms /contact.php
+++ forms /contact.php
@@ -1,59 +1,28 @@
-/*<?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  
-
-  // Replace darshil.vs23@gmail.com with your real receiving email address
-  $receiving_email_address = 'darshil.vs23@gmail.com';
-
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
-?>
-*/
-
 <?php
-//get data from form
-$name = $_POST['name'];
-$email= $_POST['email'];
-$message= $_POST['message'];
-$to = "darshil.vs23@gmail.com";
-$subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com" . "\r\n" .
-"CC: darshil.vs23@gmail.com";
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
-}
-//redirect
-//header("Location:thankyou.html");
+
+    $name=$_POST['name'];
+    $subject=$_POST['subject'];
+    $mailFrom=$_POST['email'];
+    $message=$_POST['message'];
+
+
+         $to = "darshil.vs23@gmail.com";
+         //$subject = "New message from your personal website";
+         
+         $message =  "User Name: $name.\n".
+                     //"User Phone number: $number.\n".
+                     "User Mail-ID: $mailFrom.\n".
+                     "User Message: $message. \n".
+         
+         $header = "From:darshil.vs23@gmail.com \r\n";
+         
+         
+         $retval = mail ($to,$subject,$message,$header);
+         
+         if( $retval == true ) {
+            echo "Message sent successfully...";
+         }else {
+            echo "Message could not be sent...";
+         }
+      
 ?>
